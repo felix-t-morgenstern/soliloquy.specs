fde3243bb763983f2f8cfd34be728dff6cb8c44a
felix.t.morgenstern <felix.t.morgenstern@gmail.com>
2018-12-30 16:23:43 -0500

    Changed name of IEntityUuid.createRandomEntityUuid
    Changed name of IEntityUuid.createRandomEntityUuid, to be explicit about what is being created

--- src/soliloquy/common/specs/IEntityUuidFactory.java
+++ src/soliloquy/common/specs/IEntityUuidFactory.java
@@ -26,5 +26,5 @@ public interface IEntityUuidFactory {
 	/**
 	 * Creates a new, random (and thus globally unique) EntityUuid
 	 */
-	IEntityUuid createRandomUuid();
+	IEntityUuid createRandomEntityUuid();
 }
