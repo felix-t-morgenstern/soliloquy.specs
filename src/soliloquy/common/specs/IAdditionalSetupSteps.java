cc5f67fd4b966502cf063b9ffd7e564830e72bca
felix.t.morgenstern <felix.t.morgenstern@gmail.com>
2019-02-02 20:24:28 -0500

    Added IAdditionalSetupSteps
    Added IAdditionalSetupSteps, an interface which can extend AbstractModules to support additional steps during Game setup

---
+++ src/soliloquy/common/specs/IAdditionalSetupSteps.java
@@ -0,0 +1,16 @@
+package soliloquy.common.specs;
+
+import soliloquy.game.primary.specs.IGame;
+
+/**
+ * <b>AdditionalSetupSteps</b>
+ * <p>
+ * This class contains a singular method, which acts on the Game. The intended use of this class is to be a possible extension of the AbstractModule class for each module; if the AbstractModule class implements this interface, then the Game will know to execute runAdditionalSteps during initial setup.
+ * 
+ * @author felix.t.morgenstern
+ * @version 0.0.1
+ *
+ */
+public interface IAdditionalSetupSteps {
+	void runAdditionalSteps(IGame game);
+}
