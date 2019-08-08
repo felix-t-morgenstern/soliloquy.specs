23d79dda36c3b46532e510e5a0a5406f98efab6b
felix.t.morgenstern <felix.t.morgenstern@gmail.com>
2019-08-08 01:34:06 -0400

    Refactor ResistanceCalculator into DamageResistanceCalculator and EffectResistanceCalculator
    Refactor ResistanceCalculator into DamageResistanceCalculator and EffectResistanceCalculator

--- src/soliloquy/specs/ruleset/gameconcepts/DamageResistanceCalculation.java
+++ src/soliloquy/specs/ruleset/gameconcepts/DamageResistanceCalculation.java
@@ -4,11 +4,10 @@ import soliloquy.specs.common.shared.SoliloquyClass;
 import soliloquy.specs.gamestate.entities.Character;
 import soliloquy.specs.ruleset.entities.abilities.AbilitySource;
 import soliloquy.specs.ruleset.entities.Element;
-import soliloquy.specs.ruleset.entities.StatusEffectType;
 import soliloquy.specs.ruleset.entities.VitalAttributeType;
 
 /**
- * <b>ResistanceCalculation</b>
+ * <b>DamageResistanceCalculation</b>
  * <p>
  * A heuristic for calculating the resistance a Character has to Abilities (or other events) which
  * change that Character's Status Effect or Vital Attribute levels.
@@ -21,36 +20,13 @@ import soliloquy.specs.ruleset.entities.VitalAttributeType;
  * @version 0.0.1
  *
  */
-public interface ResistanceCalculation extends SoliloquyClass {
-	/**
-	 * Calculates the effective change to a Status Effect for a specific Character, given the 
-	 * following parameters:
-	 * @param character - The Character whose resistance to calculate
-	 * @param statusEffectType - The type of the Status Effect to be effected
-	 * @param baseAmount - The amount by which the Status Effect is to be effected, prior to 
-	 * calculating resistance
-	 * @param stopAtZero - Whether the Ability intends to stop at zero. For instance, an ability
-	 * such as "Dispel Alertness" may attempt to bring Alertness down to no less than zero, even
-	 * though that StatusEffectType might permit negative values.
-	 * @param element - The Element of this effect. NB: Null values are not permitted; to have a 
-	 * "non-elemental" attack, create an element for "non-elemental" (and then perhaps implement 
-	 * this interface to ignore resistances for that Element).
-	 * @param abilitySource - The AbilitySource of this effect. May be null.
-	 * @return The effective change to this Character's Status Effect of the specified type, after
-	 * having calculated appropriate resistances.
-	 * @throws IllegalStateException If and only if the Character is dead or deleted
-	 * @throws IllegalArgumentException If and only if Character is null, StatusEffectType is null,
-	 * or element is null
-	 */
-	int calculateEffectiveChange(Character character, StatusEffectType statusEffectType,
-								 int baseAmount, boolean stopAtZero, Element element, AbilitySource abilitySource)
-					throws IllegalStateException, IllegalArgumentException;
+public interface DamageResistanceCalculation extends SoliloquyClass {
 
 	/**
 	 * Calculates the effective change to a Status Effect for a specific Character, given the 
 	 * following parameters:
 	 * @param character - The Character whose resistance to calculate
-	 * @param statusEffectType - The type of the Status Effect to be effected
+	 * @param vitalAttributeType - The type of the Vital Attribute to be effected
 	 * @param baseAmount - The amount by which the Status Effect is to be effected, prior to 
 	 * calculating resistance
 	 * @param stopAtZero - Whether the Ability intends to stop at zero. (This is unlikely to ever 
@@ -67,6 +43,7 @@ public interface ResistanceCalculation extends SoliloquyClass {
 	 * or element is null
 	 */
 	int calculateEffectiveChange(Character character, VitalAttributeType vitalAttributeType,
-								 int baseAmount, boolean stopAtZero, Element element, AbilitySource abilitySource)
+								 int baseAmount, boolean stopAtZero, Element element,
+								 AbilitySource abilitySource)
 					throws IllegalStateException, IllegalArgumentException;
 }
