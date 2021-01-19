7ee7e1c372d623a0c89076f3f67c58561ea6473b
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-01-19 00:11:11 -0500

    Test and implement GraphicsStartup
    Test and implement GraphicsStartup

--- src/soliloquy/specs/graphics/Graphics.java
+++ src/soliloquy/specs/graphics/Graphics.java
@@ -5,12 +5,24 @@ import soliloquy.specs.common.shared.SoliloquyClass;
 import soliloquy.specs.graphics.assets.Animation;
 import soliloquy.specs.graphics.assets.Sprite;
 import soliloquy.specs.graphics.assets.SpriteSet;
+import soliloquy.specs.graphics.bootstrap.GraphicsStartup;
+import soliloquy.specs.graphics.bootstrap.GraphicsTeardown;
 import soliloquy.specs.graphics.rendering.MouseCursorAsset;
 import soliloquy.specs.graphics.rendering.WindowManager;
 
 // TODO: Document this interface
 public interface Graphics extends SoliloquyClass {
 	// TODO: Just wait until the module is worked out to figure out what even goes in here
+
+    /**
+     * @return The functionality needed when setting up the graphics engine
+     */
+    GraphicsStartup startup();
+
+    /**
+     * @return The functionality needed when tearing down the graphics engine
+     */
+    GraphicsTeardown teardown();
 
     /**
      * @return A {@link Registry} of {@link Sprite}s
