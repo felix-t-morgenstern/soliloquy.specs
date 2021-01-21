51be90ae0dd38306ac62e66391b75e40eacfab21
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-01-20 22:16:07 -0500

    Test and implement GraphicsStartup
    Test and implement GraphicsStartup

---
+++ src/soliloquy/specs/graphics/rendering/WindowDisplayMode.java
@@ -0,0 +1,8 @@
+package soliloquy.specs.graphics.rendering;
+
+public enum WindowDisplayMode {
+    UNKNOWN,
+    WINDOWED,
+    WINDOWED_FULLSCREEN,
+    FULLSCREEN
+}
--- src/soliloquy/specs/graphics/rendering/WindowManager.java
+++ src/soliloquy/specs/graphics/rendering/WindowManager.java
@@ -3,46 +3,43 @@ package soliloquy.specs.graphics.rendering;
 import soliloquy.specs.common.shared.SoliloquyClass;
 import soliloquy.specs.common.valueobjects.ReadableCoordinate;
 
+/**
+ * <b>WindowManager</b>
+ * <p>
+ * This class manages the current WindowDisplayMode (i.e. Windowed, Windowed Fullscreen, or
+ * Fullscreen) of the window, and its current dimensions and location.
+ *
+ * @author felix.t.morgenstern
+ * @version 0.0.1
+ *
+ */
 public interface WindowManager extends SoliloquyClass {
     /**
-     * @return True, if and only if the window is fullscreen
+     * @return The current WindowDisplayMode
      */
-    boolean getFullscreen();
+    WindowDisplayMode getWindowDisplayMode();
+
+    /**
+     * @param windowDisplayMode The new WindowDisplayMode
+     * @throws IllegalArgumentException
+     */
+    void setWindowDisplayMode(WindowDisplayMode windowDisplayMode) throws IllegalArgumentException;
 
     /**
      * NB: Graphics engine implementation may restrict width and height combinations to common
      * screen resolutions, e.g. 800x600, 1920x1080, etc.
+     * <p>
+     * Also, this method throws an exception when the window is in windowed fullscreen mode, since
+     * the window in that case should conform to the resolution of the screen.
      * @param width The width to which to set the screen resolution
      * @param height The height to which to set the screen resolution
      * @throws IllegalArgumentException If and only if width or height are less than 1, or are
      * invalid, according to the rules of the graphics engine implementation.
+     * @throws UnsupportedOperationException If and only if current WindowDisplayMode is
+     * WindowedFullscreen
      */
-    void setFullscreen(int width, int height) throws IllegalArgumentException;
-
-    /**
-     * @return True, if and only if the window is windowed fullscreen
-     */
-    boolean getWindowedFullscreen();
-
-    /**
-     * If the window had previously been fullscreen, reverts the screen back to its original
-     * resolution, and sets the window to occupy the full screen while hiding the window bar.
-     */
-    void setWindowedFullscreen();
-
-    /**
-     * @return True, if and only if the window is windowed (i.e. a movable window, not fullscreen
-     * or windowed fullscreen)
-     */
-    boolean getWindowed();
-
-    /**
-     * @param width The width to which to set the window
-     * @param height The height to which to set the window
-     * @throws IllegalArgumentException If and only if width or height are less than 1, or less
-     * than the minimum width and height values specified by the graphics engine settings
-     */
-    void setWindowed(int width, int height) throws IllegalArgumentException;
+    void setDimensions(int width, int height)
+            throws IllegalArgumentException, UnsupportedOperationException;
 
     /**
      * NB: This method throws an exception when the window is in windowed fullscreen mode, since
@@ -51,4 +48,15 @@ public interface WindowManager extends SoliloquyClass {
      * @throws UnsupportedOperationException If and only if the window is windowed fullscreen
      */
     ReadableCoordinate windowDimensions() throws UnsupportedOperationException;
+
+    /**
+     * NB: This method will throw an exception when the {@link WindowDisplayMode} is anything but
+     * Windowed, since in the other modes, the window's location is assumed to be affixed.
+     * @param x The x position to which to set the window
+     * @param y The y position to which to set the window
+     * @throws IllegalArgumentException If and only if x or y are negative, or beyond the bounds of
+     * the screen
+     * @throws UnsupportedOperationException If the current WindowDisplayMode is not Windowed
+     */
+    void setLocation(int x, int y) throws IllegalArgumentException, UnsupportedOperationException;
 }
