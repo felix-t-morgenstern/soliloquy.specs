867580ec0e02a0a606046c82124898f4412d8148
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-04-07 19:39:15 -0400

    Test and implement Font (In progress)
    Test and implement Font (In progress)

--- src/soliloquy/specs/graphics/bootstrap/assetfactories/definitions/FontDefinition.java
+++ src/soliloquy/specs/graphics/bootstrap/assetfactories/definitions/FontDefinition.java
@@ -24,7 +24,24 @@ public interface FontDefinition extends AssetDefinition<Font> {
     float maxLosslessFontSize();
 
     /**
+     * The padding here is expressed in percentage of glyph width, so a value of 0.1f implies an
+     * additional padding of 10% of the glyph's width. <i>This method exists because some fonts
+     * require additional padding to prevent glyphs from overlapping.</i>
      * @return The amount of padding between rendered glyphs of this Font (cannot be less than 0)
      */
     float additionalGlyphPadding();
+
+    /**
+     * <i>This method exists because some TTF files do not properly encode the leading of their
+     * lines.</i> You will be able to tell this when you load the font, and you find that none of
+     * the uppercase glyphs reach the top of the line. If you want to use a font whose leading is
+     * not set properly, use this method to specify the additional percentage of the line height
+     * which should properly be the leading. If this method returns 0.1f, for instance, this
+     * implies that an additional 10% of the line height should be read by the Graphics engine as
+     * the leading of the font.
+     * <p>
+     * @see <a href="https://en.wikipedia.org/wiki/Leading">https://en.wikipedia.org/wiki/Leading</a>
+     * @return The additional percentage of the font's total line height to interpret as leading
+     */
+    float leadingAdjustment();
 }
