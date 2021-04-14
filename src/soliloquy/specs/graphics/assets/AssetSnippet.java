9911a41a37974737795ef9c05fb37f911fbc0490
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-04-14 00:49:08 -0400

    Draft a test of StackRenderer and RenderableStack which ensure that simple Sprites are rendered in the order of their z indices, and add a test which adds in the RenderingBoundaries
    Draft a test of StackRenderer and RenderableStack which ensure that simple Sprites are rendered in the order of their z indices, and add a test which adds in the RenderingBoundaries

--- src/soliloquy/specs/graphics/assets/AssetSnippet.java
+++ src/soliloquy/specs/graphics/assets/AssetSnippet.java
@@ -8,6 +8,10 @@ import soliloquy.specs.common.shared.SoliloquyClass;
  * This class defines the properties shared across all asset snippets, e.g. Sprites, single
  * Animation frames, or single ImageAssetSet snippets.
  * <p>
+ * <i>Top and left boundaries are considered inclusive, whereas bottom and right are not.</i> So,
+ * for example, if you have a 100x100 image, an AssetSnippet with boundaries of (0,0) and (100,100)
+ * would encompass the whole image; no more, and no less.
+ * <p>
  * It is intended for this interface to be implemented anonymously at runtime.
  *
  * @author felix.t.morgenstern
