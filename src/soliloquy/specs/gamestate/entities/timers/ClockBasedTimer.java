461eedc03d86900a7d95e6dc60b54fc914b982ef
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-08-22 19:48:31 -0400

    Draft class summary for ClockBasedTimer
    Draft class summary for ClockBasedTimer

--- src/soliloquy/specs/gamestate/entities/timers/ClockBasedTimer.java
+++ src/soliloquy/specs/gamestate/entities/timers/ClockBasedTimer.java
@@ -1,13 +1,24 @@
 package soliloquy.specs.gamestate.entities.timers;
 
-import soliloquy.specs.common.entities.Action;
 import soliloquy.specs.common.shared.PausableAtTime;
 import soliloquy.specs.graphics.shared.HasMostRecentTimestamp;
 
-// TODO: Draft class summary
+/**
+ * <b>ClockBasedTimer</b>
+ * <p>
+ * This class is a timer, which fires not based on how many turns have elapsed, but based on how
+ * many milliseconds have elapsed. It exposes the id of the
+ * {@link soliloquy.specs.common.entities.Action} which runs when the timer is fired, so that it
+ * can be persisted.
+ *
+ * @author felix.t.morgenstern
+ * @version 0.0.1
+ *
+ */
 public interface ClockBasedTimer extends PausableAtTime, HasMostRecentTimestamp {
     /**
-     * @return The Id of the {@link Action} which runs when this ClockBasedTimer is fired
+     * @return The Id of the {@link soliloquy.specs.common.entities.Action} which runs when this
+     * ClockBasedTimer is fired
      */
     String actionId();
 
