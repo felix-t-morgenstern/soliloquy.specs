0c62a4bdbf873ab45af2a85079d560779a6425b5
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-08-23 21:41:50 -0400

    For ActiveCharactersProvider, in both class and activeCharacters summary, specify how Characters are actually added or removed from the list of active characters
    For ActiveCharactersProvider, in both class and activeCharacters summary, specify how Characters are actually added or removed from the list of active characters

--- src/soliloquy/specs/ruleset/gameconcepts/ActiveCharactersProvider.java
+++ src/soliloquy/specs/ruleset/gameconcepts/ActiveCharactersProvider.java
@@ -10,7 +10,10 @@ import soliloquy.specs.gamestate.entities.Character;
  * <p>
  * This class provides a List of active Characters, paired with the round-specific data for
  * those Characters at the top of a round. Intended use is with
- * {@link soliloquy.specs.gamestate.entities.RoundManager#endActiveCharacterTurn}.
+ * {@link soliloquy.specs.gamestate.entities.RoundManager#endActiveCharacterTurn}. Characters are
+ * added to ActiveCharactersProvider when they are generated; and at the top of each round, by
+ * adding every Character present in the current
+ * {@link soliloquy.specs.gamestate.entities.GameZone}.
  */
 public interface ActiveCharactersProvider {
     // TODO: Specify where Characters are added or removed from the list of active Characters
@@ -19,7 +22,11 @@ public interface ActiveCharactersProvider {
      * @return A List of active Characters, in order of their turn priority, each paired with
      * round-specific data, corresponding with their data at the start of a round. (See
      * {@link soliloquy.specs.gamestate.entities.RoundManager#characterQueueRepresentation} for
-     * more information on the Character queue within a given round.
+     * more information on the Character queue within a given round. Characters are added to
+     * ActiveCharactersProvider when they are generated; and at the top of each round, by adding
+     * every Character present in the current {@link soliloquy.specs.gamestate.entities.GameZone}.
+     * It may be possible for Characters to be withheld from activeCharacters by setting some value
+     * in their data, but that is specific to an individual ruleset.
      */
     List<Pair<Character, VariableCache>> activeCharacters();
 }
