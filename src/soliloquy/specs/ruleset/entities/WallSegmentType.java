17ff8dfdd27b5a0c5189a932b4e6816d4f3a7e4d
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-08-25 22:40:45 -0400

    Draft class and method summaries for WallSegmentType
    Draft class and method summaries for WallSegmentType

--- src/soliloquy/specs/ruleset/entities/WallSegmentType.java
+++ src/soliloquy/specs/ruleset/entities/WallSegmentType.java
@@ -5,15 +5,45 @@ import soliloquy.specs.gamestate.entities.Tile;
 import soliloquy.specs.gamestate.entities.TileWallSegment;
 import soliloquy.specs.graphics.assets.ImageAsset;
 
-// TODO: Document (and perhaps expound upon) this interface
+/**
+ * <b>WallSegmentType</b>
+ * <p>
+ * This is a type of {@link TileWallSegment}, which exists on a {@link Tile}, at a given height. It
+ * can block progress north (e.g. a wall on the north edge of a Tile), west (e.g. a wall on the
+ * west edge of a Tile), or northwest (e.g. a corner piece, preventing diagonal movement). It has
+ * an ImageAssetSet, allowing it to support things like whether it is fully vertically visible, or
+ * whether only a small portion is displayed, to prevent the wall from obscuring the player's view
+ * of the room the party is in.
+ * <p>
+ * It is worth noting that TileWallSegments can only be on the west, northwest, or north edges of a
+ * Tile. If you want a TileWallSegment on the south edge of a Tile, place it on the north edge of
+ * the Tile to the south of that initial Tile.
+ *
+ * @author felix.t.morgenstern
+ * @version 0.0.1
+ *
+ */
 public interface WallSegmentType extends HasId {
-	TileWallSegment generate(Tile tile);
-
+	/**
+	 * This method allows this class to support things like whether it is fully vertically visible,
+	 * or whether only a small portion is displayed, to prevent the wall from obscuring the
+	 * player's view of the room the party is in.
+	 * @return The ImageAssetSet for this WallSegmentType
+	 */
 	ImageAsset imageAsset();
 
+	/**
+	 * @return True, if and only if a TileWallSegment of this type blocks movement to the west
+	 */
 	boolean blocksWest();
 
+	/**
+	 * @return True, if and only if a TileWallSegment of this type blocks movement to the northwest
+	 */
 	boolean blocksNorthwest();
 
+	/**
+	 * @return True, if and only if a TileWallSegment of this type blocks movement to the north
+	 */
 	boolean blocksNorth();
 }
