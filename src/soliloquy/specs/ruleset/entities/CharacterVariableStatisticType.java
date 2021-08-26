48a30380e2d421c3f92d72047ae4539a31e57ceb
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-08-25 22:29:32 -0400

    Revise class summary for CharacterVariableStatisticType
    Revise class summary for CharacterVariableStatisticType

--- src/soliloquy/specs/ruleset/entities/CharacterVariableStatisticType.java
+++ src/soliloquy/specs/ruleset/entities/CharacterVariableStatisticType.java
@@ -2,14 +2,14 @@ package soliloquy.specs.ruleset.entities;
 
 import soliloquy.specs.common.shared.HasPluralName;
 
-// TODO: Revise this documentation
 /**
  * <b>CharacterVariableStatisticType</b>
  * <p>
- * It has a name, a pluralized name, and an Id.
+ * It has a name, a pluralized name, a way to obtain icons for Characters (e.g. for status
+ * indicators on a {@link soliloquy.specs.gamestate.entities.Party} member's portrait), and an Id.
  * <p>
- * It has Actions, which can be performed on each Character, at the start and end of their turn,
- * and the start and end of the round.
+ * It has behaviors which can be performed on each Character at the start or end of their turn or
+ * the round, e.g. regeneration, taking damage from poison, spilling blood on the ground, etc.
  * 
  * @author felix.t.morgenstern
  * @version 0.0.1
