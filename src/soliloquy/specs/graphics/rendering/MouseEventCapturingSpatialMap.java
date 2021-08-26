ca50fc98d7aaa9e681502fe9e14720b0a4fa7bd9
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-08-26 00:16:51 -0400

    Define MouseEventCapturingSpatialMap
    Define MouseEventCapturingSpatialMap

--- src/soliloquy/specs/graphics/rendering/MouseEventCapturingSpatialMap.java
+++ src/soliloquy/specs/graphics/rendering/MouseEventCapturingSpatialMap.java
@@ -1,6 +1,7 @@
 package soliloquy.specs.graphics.rendering;
 
 import soliloquy.specs.common.shared.SoliloquyClass;
+import soliloquy.specs.graphics.renderables.RenderableWithArea;
 
 /**
  * <b>MouseEventCapturingSpatialMap</b>
@@ -15,5 +16,32 @@ import soliloquy.specs.common.shared.SoliloquyClass;
  *
  */
 public interface MouseEventCapturingSpatialMap extends SoliloquyClass {
-    // TODO: Fill in
+    /**
+     * @param x The x location in the window, ranging from 0.0 to 1.0, to check for a Renderable
+     *          which captures mouse events
+     * @param y The y location in the window, ranging from 0.0 to 1.0, to check for a Renderable
+     *          which captures mouse events
+     * @return The RenderableWithArea capturing mouse events at that particular location; if no
+     * Renderable is capturing events, this method returns null
+     * @throws IllegalArgumentException If and only if x or y are outside of the range of [0.0,
+     * 1.0]
+     */
+    RenderableWithArea getCapturingRenderableAtPoint(float x, float y)
+            throws IllegalArgumentException;
+
+    /**
+     * If renderable is already present in this map, its location is updated
+     * @param renderable The Renderable to place in this map
+     * @throws IllegalArgumentException If and only if renderable is null, or does not capture
+     * mouse events
+     */
+    void putRenderable(RenderableWithArea renderable) throws IllegalArgumentException;
+
+    /**
+     * (NB: This method does not require renderable to capture mouse events, since it can be used
+     * to remove Renderables which are altered to no longer capture mouse events.)
+     * @param renderable The Renderable to remove from this map
+     * @throws IllegalArgumentException If and only if renderable is null
+     */
+    void removeRenderable(RenderableWithArea renderable) throws IllegalArgumentException;
 }
