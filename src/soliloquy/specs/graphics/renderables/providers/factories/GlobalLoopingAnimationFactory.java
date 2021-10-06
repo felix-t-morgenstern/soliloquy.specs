66e0475c4cbcabbef161546736c00c99c50d95c7
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-10-06 00:31:58 -0400

    Refactor GlobalLoopingAnimationImpl to receive pausedTimestamp in constructor; add necessary unit tests, and also refactor factory accordingly
    Refactor GlobalLoopingAnimationImpl to receive pausedTimestamp in constructor; add necessary unit tests, and also refactor factory accordingly

--- src/soliloquy/specs/graphics/renderables/providers/factories/GlobalLoopingAnimationFactory.java
+++ src/soliloquy/specs/graphics/renderables/providers/factories/GlobalLoopingAnimationFactory.java
@@ -22,10 +22,13 @@ public interface GlobalLoopingAnimationFactory extends SoliloquyClass {
      * @param animation The Animation to use for this GlobalLoopingAnimation
      * @param periodModuloOffset The milliseconds by which the period of this Animation is offset
      *                           (c.f. {@link soliloquy.specs.common.shared.HasPeriodModuloOffset})
+     * @param pauseTimestamp The timestamp at which this GlobalLoopingAnimation has been paused; if
+     *                       unpaused, this value should be null
      * @return The newly-created GlobalLoopingAnimation
      * @throws IllegalArgumentException If and only if uuid is null, animation is null, or
      * periodModuloOffset is negative or greater than or equal to the ms duration of animation
      */
-    GlobalLoopingAnimation make(String id, Animation animation, int periodModuloOffset)
+    GlobalLoopingAnimation make(String id, Animation animation, int periodModuloOffset,
+                                Long pauseTimestamp)
             throws IllegalArgumentException;
 }
