18d32b52347ed3e4061037cda3dc981814345bf2
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2023-02-08 19:53:59 -0500

    refactor: Refactor StatisticMagnitudeEffectCalculation to calculate for Static statistics; and TurnHandling and RoundEndHandling to use Static statistic Effects; also javadocs descriptions for Effects

--- src/main/java/soliloquy/specs/ruleset/gameconcepts/StatisticMagnitudeEffectCalculation.java
+++ src/main/java/soliloquy/specs/ruleset/gameconcepts/StatisticMagnitudeEffectCalculation.java
@@ -3,6 +3,7 @@ package soliloquy.specs.ruleset.gameconcepts;
 import soliloquy.specs.gamestate.entities.Character;
 import soliloquy.specs.gamestate.entities.exceptions.EntityDeletedException;
 import soliloquy.specs.ruleset.entities.actonroundendandcharacterturn.StatisticChangeMagnitude;
+import soliloquy.specs.ruleset.entities.character.CharacterStaticStatisticType;
 import soliloquy.specs.ruleset.entities.character.CharacterVariableStatisticType;
 import soliloquy.specs.ruleset.entities.character.StatusEffectType;
 
@@ -19,6 +20,26 @@ import soliloquy.specs.ruleset.entities.character.StatusEffectType;
  * @version 0.0.1
  */
 public interface StatisticMagnitudeEffectCalculation {
+    /**
+     * Calculates an ultimate statistic change effect for a change of a given magnitude, after
+     * potentially taking into account resistances (see
+     * {@link VariableStatisticLossResistanceCalculation} and
+     * {@link StatusEffectResistanceCalculation}).
+     * <p>
+     * This method is only for StatisticChangeMagnitudes of {@link CharacterVariableStatisticType}s.
+     *
+     * @param staticStatisticType The {@link CharacterVariableStatisticType} of the magnitude
+     * @param magnitude           The magnitude of statistic change applied to the Character
+     * @param character           The Character to whom the effect will transpire
+     * @return The net statistic change effect
+     * @throws IllegalArgumentException If and only if staticStatisticType is null, or magnitude
+     *                                  is null or invalid, or character is null
+     * @throws EntityDeletedException   If and only if character is deleted
+     */
+    <T extends Number> int getEffect(CharacterStaticStatisticType staticStatisticType,
+                                     StatisticChangeMagnitude<T> magnitude, Character character)
+            throws IllegalArgumentException, EntityDeletedException;
+
     /**
      * Calculates an ultimate statistic change effect for a change of a given magnitude, after
      * potentially taking into account resistances (see
