2df6bbc8b33aa00c24bbb1719ccc07d8bdb0e9ef
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-07-17 22:45:17 -0400

    refactor: Rename VALUES_AT_TIMESTAMPS to more correct name

--- src/main/java/soliloquy/specs/ui/definitions/providers/FiniteLinearMovingProviderDefinition.java
+++ src/main/java/soliloquy/specs/ui/definitions/providers/FiniteLinearMovingProviderDefinition.java
@@ -3,11 +3,11 @@ package soliloquy.specs.ui.definitions.providers;
 import soliloquy.specs.common.valueobjects.Pair;
 
 public class FiniteLinearMovingProviderDefinition<T> extends AbstractProviderDefinition<T> {
-    public final Pair<Integer, T>[] VALUES_AT_TIMESTAMPS;
+    public final Pair<Integer, T>[] VALUES_AT_TIMESTAMP_OFFSETS;
 
     @SafeVarargs
     protected FiniteLinearMovingProviderDefinition(Pair<Integer, T>... valuesAtTimestampOffsets) {
-        VALUES_AT_TIMESTAMPS = valuesAtTimestampOffsets;
+        VALUES_AT_TIMESTAMP_OFFSETS = valuesAtTimestampOffsets;
     }
 
     @SafeVarargs
