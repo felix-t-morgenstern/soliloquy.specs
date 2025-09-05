65c60066baa5334dec9cfa182283643c74ea1bff
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-09-05 16:52:20 -0400

    feat: Test and implement ComponentFactory

--- src/main/java/soliloquy/specs/io/graphics/renderables/factories/ComponentFactory.java
+++ src/main/java/soliloquy/specs/io/graphics/renderables/factories/ComponentFactory.java
@@ -1,22 +1,30 @@
 package soliloquy.specs.io.graphics.renderables.factories;
 
 import soliloquy.specs.io.graphics.renderables.Component;
-import soliloquy.specs.io.input.keyboard.entities.KeyBindingContext;
 
 import java.util.UUID;
 
 /**
  * <b>ComponentFactory</b>
  * <p>
- * Creates {@link Component}s
+ * Creates {@link Component}s. (This factory is exceedingly straightforward, but it needs to be
+ * exposed to other modules.)
  *
  * @author felix.t.morgenstern
  * @version 0.0.1
  */
 public interface ComponentFactory {
+    /**
+     * @param uuid                The uuid of the Component
+     * @param z                   The z-index of the Component
+     * @param containingComponent The Component which contains the newly-created Component, may be
+     *                            null
+     * @return The newly-created Component
+     * @throws IllegalArgumentException If and only if uuid is null
+     */
     Component make(
             UUID uuid,
             int z,
-            KeyBindingContext bindingContext
+            Component containingComponent
     ) throws IllegalArgumentException;
 }
