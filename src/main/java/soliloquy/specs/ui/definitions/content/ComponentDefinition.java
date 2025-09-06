6720021552971ff189ff7bcf8156744959746493
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-09-06 19:46:45 -0400

    feat: Refactor ContentDefinitionReader to read all Renderable definitions

--- src/main/java/soliloquy/specs/ui/definitions/content/ComponentDefinition.java
+++ src/main/java/soliloquy/specs/ui/definitions/content/ComponentDefinition.java
@@ -1,20 +1,32 @@
 package soliloquy.specs.ui.definitions.content;
 
+import soliloquy.specs.common.valueobjects.FloatBox;
 import soliloquy.specs.ui.definitions.keyboard.KeyBindingDefinition;
+import soliloquy.specs.ui.definitions.providers.AbstractProviderDefinition;
 
 public class ComponentDefinition extends AbstractContentDefinition {
+    public final AbstractProviderDefinition<FloatBox> DIMENSIONS_PROVIDER;
     public final AbstractContentDefinition[] CONTENT;
 
     public KeyBindingDefinition[] bindings;
     public Boolean blocksLowerBindings;
 
-    private ComponentDefinition(int z, AbstractContentDefinition... content) {
+    private ComponentDefinition(
+            int z,
+            AbstractProviderDefinition<FloatBox> dimensProvider,
+            AbstractContentDefinition... content
+    ) {
         super(z);
+        DIMENSIONS_PROVIDER = dimensProvider;
         CONTENT = content;
     }
 
-    public static ComponentDefinition component(int z, AbstractContentDefinition... content) {
-        return new ComponentDefinition(z, content);
+    public static ComponentDefinition component(
+            int z,
+            AbstractProviderDefinition<FloatBox> dimensProvider,
+            AbstractContentDefinition... content
+    ) {
+        return new ComponentDefinition(z, dimensProvider, content);
     }
 
     public ComponentDefinition withBindings(boolean blocksLowerBindings,
