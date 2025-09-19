3aeabf7f70686921e26b9e4be6fbfe7dd44f8c9e
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-09-19 15:10:46 -0400

    feat: Define, test, implement, and register FunctionalProviderFactory

--- src/main/java/soliloquy/specs/io/graphics/renderables/providers/factories/FunctionalProviderFactory.java
+++ src/main/java/soliloquy/specs/io/graphics/renderables/providers/factories/FunctionalProviderFactory.java
@@ -2,6 +2,9 @@ package soliloquy.specs.io.graphics.renderables.providers.factories;
 
 import soliloquy.specs.io.graphics.renderables.providers.FunctionalProvider;
 
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * <b>FunctionalProviderFactory</b>
  * <p>
@@ -13,6 +16,7 @@ import soliloquy.specs.io.graphics.renderables.providers.FunctionalProvider;
  */
 public interface FunctionalProviderFactory {
     /**
+     * @param uuid              The UUID of this Provider
      * @param provideFunctionId The Id of the {@link soliloquy.specs.common.entities.Function} which
      *                          provides the value in question
      * @param pauseActionId     The Id of an {@link soliloquy.specs.common.entities.Action} which
@@ -22,12 +26,20 @@ public interface FunctionalProviderFactory {
      * @param unpauseActionId   Same as above, but for when
      *                          {@link soliloquy.specs.common.shared.PausableAtTime#reportPause} is
      *                          called
+     * @param pauseTimestamp    The timestamp at which this Provider has been paused, may be null
+     * @param data              The data for this provider, fed into the provide, pause, and action
+     *                          Function and Actions as part of a
+     *                          {@link FunctionalProvider.EventInfo}
      * @param <T>               The type provided
      * @return The newly-created FunctionalProvider
-     * @throws IllegalArgumentException If and only if provideFunctionId is null; or if
+     * @throws IllegalArgumentException If and only if provideFunctionId or data are null; or if
      *                                  provideFunctionId, pauseActionId, or unpauseActionId are
      *                                  empty or do not correspond to valid entities
      */
-    <T> FunctionalProvider<T> make(String provideFunctionId, String pauseActionId,
-                                   String unpauseActionId) throws IllegalArgumentException;
+    <T> FunctionalProvider<T> make(UUID uuid,
+                                   String provideFunctionId,
+                                   String pauseActionId,
+                                   String unpauseActionId,
+                                   Long pauseTimestamp,
+                                   Map<String, Object> data) throws IllegalArgumentException;
 }
