ff50554268d974ff4d12616d1ce165606fc60f7a
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-09-20 01:01:18 -0400

    feat: Test, implement, and register FunctionalProviderDefinitionReader

---
+++ src/main/java/soliloquy/specs/ui/definitions/providers/FunctionalProviderDefinition.java
@@ -0,0 +1,48 @@
+package soliloquy.specs.ui.definitions.providers;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class FunctionalProviderDefinition<T> extends AbstractProviderDefinition<T> {
+    public final String PROVIDE_FUNCTION_ID;
+
+    public UUID uuid;
+    public String pauseActionId;
+    public String unpauseActionId;
+    public Long pauseTimestamp;
+    public Map<String, Object> data;
+
+    private FunctionalProviderDefinition(String provideFunctionId) {
+        PROVIDE_FUNCTION_ID = provideFunctionId;
+    }
+
+    public static <T> FunctionalProviderDefinition<T> functionalProvider(String provideFunctionId) {
+        return new FunctionalProviderDefinition<>(provideFunctionId);
+    }
+
+    public FunctionalProviderDefinition<T> withUuid(UUID uuid) {
+        this.uuid = uuid;
+
+        return this;
+    }
+
+    public FunctionalProviderDefinition<T> withPauseActions(String pauseActionId,
+                                                            String unpauseActionId) {
+        this.pauseActionId = pauseActionId;
+        this.unpauseActionId = unpauseActionId;
+
+        return this;
+    }
+
+    public FunctionalProviderDefinition<T> withPauseTimestamp(Long pauseTimestamp) {
+        this.pauseTimestamp = pauseTimestamp;
+
+        return this;
+    }
+
+    public FunctionalProviderDefinition<T> withData(Map<String, Object> data) {
+        this.data = data;
+
+        return this;
+    }
+}
